062fb730568c598890592124c8a1eb1dfbf79453
PedroAlvesPaula <pedropaula2325p@gmail.com>
2025-06-18 11:02:17 -0300

    refact: troca nome da rota e nome e icone da barra de tarefas

--- imports/modules/toDo/config/toDoAppMenu.tsx
+++ imports/modules/toDo/config/toDoAppMenu.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { IAppMenu } from '/imports/modules/modulesTypings';
-import SysIcon from '/imports/ui/components/sysIcon/sysIcon';
+import ListAltIcon from '@mui/icons-material/ListAlt';
 
 export const toDoMenuItemList: (IAppMenu | null)[] = [
 	{
-		path: '/toDo',
-		name: 'ToDo',
-		icon: <SysIcon name={'dashboard'} />
+		path: '/toDo/tasks',
+		name: 'Lista de Tareas',
+		icon: <ListAltIcon />
 	}
 ];
