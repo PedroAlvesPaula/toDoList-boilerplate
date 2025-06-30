312798dde4bba12aab4e464c9177e3311bf85f4c
PedroAlvesPaula <pedropaula2325p@gmail.com>
2025-06-30 08:11:46 -0300

    refact: retira imports nao utilizados

--- imports/sysPages/config/pagesrouters.tsx
+++ imports/sysPages/config/pagesrouters.tsx
@@ -1,4 +1,3 @@
-import Home from '../../sysPages/pages/home/home';
 import { SignUp } from '../../sysPages/pages/signUp/signUp';
 import { EmailVerify } from '../../sysPages/pages/emailVerify/emailVerify';
 import { ResetPassword } from '../../sysPages/pages/resetPassword/resetPassword';
@@ -6,8 +5,6 @@ import { PasswordRecovery } from '../../sysPages/pages/recoveryPassword/password
 import { IRoute } from '../../modules/modulesTypings';
 import { NoPermission } from '../../sysPages/pages/noPermission/noPermission';
 import SignInPage from '../pages/signIn/signIn';
-import { HomeResources, SysFormTestPageResources } from './resources';
-import SysFormPlayground from '../../sysPages/pages/sysFormPlayground/sysFormPlayground';
 
 export const pagesRouterList: (IRoute | null)[] = [
 	// {
