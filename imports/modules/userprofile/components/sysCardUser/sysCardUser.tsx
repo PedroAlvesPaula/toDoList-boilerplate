505d4221b52d0dd2d93821afca9836300e2d938b
PedroAlvesPaula <pedropaula2325p@gmail.com>
2025-07-03 13:14:19 -0300

    refact: retorna os arquivos das paginas de usuario

--- imports/modules/userprofile/components/sysCardUser/sysCardUser.tsx
+++ imports/modules/userprofile/components/sysCardUser/sysCardUser.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import { Avatar, SxProps, Table, TableBody, Theme } from '@mui/material';
+import { SxProps, Theme } from '@mui/material';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import SysCardUserStyled from './sysCardUserStyles';
 import { UserProfileListControllerContext } from '../../pages/UserProfileList/userProfileListController';
 import SysIcon from '../../../../ui/components/sysIcon/sysIcon';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import EmailIcon from '@mui/icons-material/Email';
-import CakeIcon from '@mui/icons-material/Cake';
-import WcIcon from '@mui/icons-material/Wc';
-import BusinessIcon from '@mui/icons-material/Business';
 
 interface ISysCardUserProps {
 	username: string;
 	roles?: string[];
 	email: string;
+	status: string | undefined;
 	userId: string;
 	sx?: SxProps<Theme>;
 }
@@ -23,59 +20,52 @@ interface ISysCardUserProps {
 export const SysCardUser: React.FC<ISysCardUserProps> = ({ ...props }: ISysCardUserProps) => {
 	const context = React.useContext(UserProfileListControllerContext);
 	const { translateStatus, onChangeStatusClick, onEdit } = context;
-	const { username, roles, email, userId, sx } = props;
+	const { username, roles, email, status, userId, sx } = props;
 
-	const { Container, PaperContainer, TableContainer, RowTable, CellTable } = SysCardUserStyled;
+	const { Container, ActionBox, Status } = SysCardUserStyled;
 
 	return (
-		<Container>
-			<PaperContainer elevation={4}>
-				<TableContainer>
-					<Avatar src="#" alt="Foto de perfil" sx={{ width: 100, height: 100, marginTop: '16px' }} />
-					<Typography variant="h5" fontWeight="bold" marginTop="16px">
-						Username
-					</Typography>
-					<Table>
-						<TableBody>
-							<RowTable>
-								<CellTable align="center">
-									<EmailIcon fontSize="large" />
-								</CellTable>
-								<CellTable align="center">
-									<Typography variant="body1">{email}</Typography>
-								</CellTable>
-							</RowTable>
-
-							<RowTable>
-								<CellTable align="center">
-									<CakeIcon fontSize="large" />
-								</CellTable>
-								<CellTable align="center">
-									<Typography variant="body1">Aniversario</Typography>
-								</CellTable>
-							</RowTable>
-
-							<RowTable>
-								<CellTable align="center">
-									<WcIcon fontSize="large" />
-								</CellTable>
-								<CellTable align="center">
-									<Typography variant="body1">Gender</Typography>
-								</CellTable>
-							</RowTable>
-
-							<RowTable>
-								<CellTable align="center">
-									<BusinessIcon fontSize="large" />
-								</CellTable>
-								<CellTable align="center">
-									<Typography variant="body1">Empresa</Typography>
-								</CellTable>
-							</RowTable>
-						</TableBody>
-					</Table>
-				</TableContainer>
-			</PaperContainer>
+		<Container sx={sx} key={userId}>
+			<Typography sx={{ gridArea: 'name' }} variant="subtitle1">
+				{username}
+			</Typography>
+			<Box sx={{ gridArea: 'roles' }}>
+				{roles?.map((role) => {
+					return (
+						<Typography key={role} variant="body1">
+							{role}
+						</Typography>
+					);
+				})}
+			</Box>
+			<Typography variant="body1" sx={{ gridArea: 'email' }}>
+				{email}
+			</Typography>
+			<Status color={status === 'active' ? 'primary.main' : 'sysText.disabled'} variant="body1">
+				{translateStatus(status)}
+			</Status>
+			<ActionBox>
+				{status === 'active' ? (
+					<>
+						<Tooltip title={'Destivar'}>
+							<IconButton onClick={() => onChangeStatusClick(userId!)}>
+								<SysIcon name={'doNotDisturbOn'} />
+							</IconButton>
+						</Tooltip>
+						<Tooltip title={'Editar'}>
+							<IconButton onClick={() => onEdit(userId)}>
+								<SysIcon name={'edit'} />
+							</IconButton>
+						</Tooltip>
+					</>
+				) : (
+					<Tooltip title={'Ativar'}>
+						<IconButton onClick={() => onChangeStatusClick(userId!)}>
+							<SysIcon name={'checkCircle'} />
+						</IconButton>
+					</Tooltip>
+				)}
+			</ActionBox>
 		</Container>
 	);
 };
--- imports/modules/userprofile/components/sysCardUser/sysCardUserStyles.tsx
+++ imports/modules/userprofile/components/sysCardUser/sysCardUserStyles.tsx
@@ -1,47 +1,55 @@
 import React from 'react';
-import Box, { BoxProps } from '@mui/material/Box';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
-import { PaperProps, TableCell, TableCellProps, TableRow, TableRowProps } from '@mui/material';
-import Paper from '@mui/material/Paper';
+import { sysShadows, sysSizing } from '../../../../ui/materialui/styles';
 
 interface ISysCardUserStyled {
-	Container: React.ElementType<BoxProps>;
-	PaperContainer: React.ElementType<PaperProps>;
-	TableContainer: React.ElementType<BoxProps>;
-	RowTable: React.ElementType<TableRowProps>;
-	CellTable: React.ElementType<TableCellProps>;
+	Container: React.ElementType;
+	ActionBox: React.ElementType;
+	Status: React.ElementType;
 }
 
 const SysCardUserStyled: ISysCardUserStyled = {
 	Container: styled(Box)(({ theme }) => ({
 		width: '100%',
-		display: 'flex',
-		flexDirection: 'column',
+		backgroundColor: theme.palette.background.default,
+		borderRadius: sysSizing.radiusSm,
+		padding: sysSizing.spacingFixedMd,
+		boxShadow: sysShadows.shadow2,
+		display: 'grid',
+		gap: '0.75rem 1.25rem',
+		gridTemplateColumns: '2fr 1fr 2fr 140px 64px',
+		gridTemplateAreas: '"name roles email status actions"',
 		alignItems: 'center',
-		marginTop: '32px'
-	})),
-
-	PaperContainer: styled(Paper)(({ theme }) => ({
-		minWidth: 450,
-		margin: 'auto',
-		m: 4,
-		p: 3,
-		borderRadius: 3,
+		[theme.breakpoints.down('lg')]: {
+			gridTemplateColumns: '2fr 1fr 64px',
+			gridTemplateAreas: '"name roles actions" "email status status"'
+		},
 		[theme.breakpoints.down('sm')]: {
-			minWidth: '90%'
+			gridTemplateColumns: '1fr 1fr 64px',
+			gridTemplateAreas: '"name name actions" "email email email" "roles status status"'
+		},
+		'& > p': {
+			wordBreak: 'break-all'
 		}
 	})),
-	TableContainer: styled(Box)(({ theme }) => ({
+	ActionBox: styled(Box)(({ theme }) => ({
+		gridArea: 'actions',
 		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-		gap: 2
+		justifyContent: 'end',
+		gap: sysSizing.spacingFixedMd,
+		'> svg': {
+			cursor: 'pointer',
+			color: theme.palette.sysAction?.primaryIcon
+		}
 	})),
-	RowTable: styled(TableRow)(({ theme }) => ({
-		width: '100%',
-		justifyContent: 'space-between'
-	})),
-	CellTable: styled(TableCell)(({ theme }) => ({}))
+	Status: styled(Typography)(({ theme }) => ({
+		gridArea: 'status',
+		[theme.breakpoints.down('sm')]: {
+			justifySelf: 'end'
+		}
+	}))
 };
 
 export default SysCardUserStyled;
