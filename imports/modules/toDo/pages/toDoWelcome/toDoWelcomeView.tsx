e99394fb10458e1007031e172afb214fc1455e5b
PedroAlvesPaula <pedropaula2325p@gmail.com>
2025-07-03 11:18:13 -0300

    feat: adiciona novos elementos a tela de boas vindas

--- imports/modules/toDo/pages/toDoWelcome/toDoWelcomeView.tsx
+++ imports/modules/toDo/pages/toDoWelcome/toDoWelcomeView.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import toDoWelcomeStyles from './toDoWelcomeStyles';
 import { toDoWelcomeControlerContext } from './toDoWelcomeController';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import { ListItem, ListItemText } from '@mui/material';
+import { Box, Divider, ListItem, ListItemText, Typography } from '@mui/material';
 import { SysButton } from '/imports/ui/components/SimpleFormFields/SysButton/SysButton';
+import AuthContext from '/imports/app/authProvider/authContext';
+import { TasksCard } from '../../components/tasksCard';
 
 const ToDoWelcomeView = () => {
 	const context = React.useContext(toDoWelcomeControlerContext);
 
-	const { Container, Card, CardContent, Title, Description } = toDoWelcomeStyles;
+	const { user } = React.useContext(AuthContext);
+
+	const { Container, HeaderContainer, TasksContainer, LastAdded, LastAddedTask } = toDoWelcomeStyles;
 
 	return (
 		<Container>
-			<Title variant="h2">Atividades recentes</Title>
+			<HeaderContainer>
+				<Typography variant="h2" fontSize="3.5rem">
+					Olá, {user?.username || 'Sem nome'}
+				</Typography>
+				<Typography component="p" fontSize="1.1rem" sx={{ margin: '16px 0' }}>
+					Seus Projetos mais organizados. Veja as tarefas adicionadas por seu time, por você e para você!
+				</Typography>
+			</HeaderContainer>
+
+			<Divider variant="inset" component="li" sx={{ listStyle: 'none', width: '100%' }} />
+			<TasksContainer>
+				<LastAdded>
+					<Typography component="p" fontSize="1.1rem" flexDirection="row" fontWeight="bold">
+						Adicionadas Recentemente
+					</Typography>
+					<LastAddedTask>
+						<TasksCard tasks={[context.lastTaskAdded]} hideMenu={true} />
+					</LastAddedTask>
+				</LastAdded>
+				<TasksCard tasks={context.lastTasks} hideMenu={true} />
+			</TasksContainer>
+
 			<SysButton
 				onClick={() => context.navigate('/toDo/tasks')}
 				size="small"
@@ -20,29 +44,6 @@ const ToDoWelcomeView = () => {
 				stylevariant="secondary">
 				Minhas tarefas
 			</SysButton>
-			{context.fiveLastTasks.map((item, index) => (
-				<Card key={index}>
-					<CardContent
-						sx={(theme) => ({
-							backgroundColor: item.isCompleted ? theme.palette.tertiary.light : theme.palette.secondary.main
-						})}>
-						<ListItem sx={{ textAlign: 'center' }}>
-							<ListItemText
-								primary={
-									<Title variant="h5" component="div">
-										{item.title}
-									</Title>
-								}
-								secondary={
-									<Description variant="body2" color="text.secondary" component={'span'}>
-										{item.isCompleted ? 'Tarefa concluída' : 'Tarefa pendente'}
-									</Description>
-								}
-							/>
-						</ListItem>
-					</CardContent>
-				</Card>
-			))}
 		</Container>
 	);
 };
